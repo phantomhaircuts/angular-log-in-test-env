f5dd0a45692ac54ba53b381fc6bde5e6dff8ce5d
Erik Anderson <itserikanderson@gmail.com>
2015-10-19 15:01:05 -0400

    session in session Storage

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -1,14 +1,14 @@
 var app = angular.module("login", [
   'angular-md5',
+  'ngStorage',
   'authentication'
 ]);
 
 angular.module('authentication', ['ngStorage'])
-.controller("loginController", ['$scope', '$http', 'md5', function($scope, $http, md5, $localStorage,
-    $sessionStorage) {
-
+.controller("loginController", ['$scope', '$http', 'md5', function($scope, $http, md5, $localStorage, $sessionStorage) {
+  $scope.$storage = $localStorage
   //Pass local Storage by reference to a hook under scope
-  $scope.$storage = $localStorage;
+
 
   // This code toggles the form on clicking the signin button
   this.formIsVisible = false
@@ -34,8 +34,8 @@ angular.module('authentication', ['ngStorage'])
   //Submit password and username
   $scope.loginCtrl.submit = function() {
     //declare variables for authentication
-    var username = $scope.loginCtrl.session.username
-    var password = $scope.loginCtrl.session.password
+    username = $scope.loginCtrl.session.username
+    password = $scope.loginCtrl.session.password
     var xdate = new Date();
     var auth = md5.createHash(password + xdate);
     xauthentication = username + ":" + auth;
@@ -60,17 +60,22 @@ angular.module('authentication', ['ngStorage'])
 
     //HTTP request
     $http(req)
-    .then(function successCallback ( response, status ) {
+    .then(function successCallback ( response ) {
       console.log("You are logged in!")
       console.log( "This is response status: "+ response.status );
       console.dir(response);
       //Create Session and Store XAUTHENTICATION
       if (response.status == 200) {
-        console.log("Session Created " + xauthentication);
+        $scope.saveData = function(){
+        sessionStorage.setItem(username, password);
+        console.log('session stored')
+        console.log( 'hello ' + sessionStorage.getItem(username))
+        }();
       }
 
     }, function errorCallback(response) {
       console.log("Check your Username or Password")
+      console.log( "This is response status: "+ response.status );
       alert("failed!")
     // called asynchronously if an error occurs
     // or server returns response with an error status.
