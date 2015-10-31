0400fc9934e128deb90c617b9120d1eb2281892c
Erik Anderson <itserikanderson@gmail.com>
2015-10-31 13:05:04 -0400

    uploads image to IMGUR

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,7 @@
     'onboardingLogin',
     'onboardingDirectives',
     'ngAnimate',
-    'ngResource'
+    'ngResource',
+    'naif.base64'
   ]);
 })()
--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -4,7 +4,8 @@ var app = angular.module("onboardingApp", [
   'authentication',
   'ui.router',
   'ngAnimate',
-  'ngResource'
+  'ngResource',
+  'naif.base64'
 ]);
 
 // UI ROUTER CODE =============================================
@@ -290,8 +291,8 @@ function getUser(){
 
     // FRESH DESK FOR HANDLING PHOTOS AND CREATING TICKETS
     var apiKey = 'Duw1oQU6YazvCtT5cynJ:x';
-    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-    var freshKey = Base64.encode(apiKey);
+    Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+    freshKey = Base64.encode(apiKey);
     var freshData = {
       'helpdesk_ticket':{
         'group_id': 5000252927,
@@ -299,12 +300,13 @@ function getUser(){
         'email': 'example@example.com',
         'subject': 'TICKET TEST',
         'description': "Location #" + locId + " " + locName + " has been updated via the Setup Tool by " + username + " \n \n Address:\n" + locAddress + "\n" + locAddressTwo + "\n" + locCity + ", " + locState + ", " + locZip + "\nMake: " + locMake + "\nModel: " + locModel + "\nFirmware: " + locFirmware + "\n\n Par Values: \n" + locPar,
-        // 'attachments': {'resource': {'file': fileVar, content_type:'image/png'}}
+        // 'attachments': {'resource': {'file': filePayload, content_type:'image/png'}}
         // '[attachments][][resource]': {file: 'logo.png', content_type: 'image/png'}
-      }
+      },
+      'attachement': {filePayload}
     };
-    var freshEnd = 'vagabondvending.freshdesk.com';
-    var freshReq = {
+      freshEnd = 'vagabondvending.freshdesk.com';
+      freshReq = {
       method: 'POST',
       url: "https://" + freshEnd + "/helpdesk/tickets.json",
       data: freshData,
@@ -317,11 +319,75 @@ function getUser(){
     }
     $http(freshReq)
     .then(function freshCallback ( response, data ) {
-      console.log("fresh is being submitted")
+      console.log("freshdesk is being submitted")
       if (response.status == 200) {
-        console.log("fresh was Successful!")
+        console.log("freshdesk was Successful!")
         console.log(freshKey)
       };
     });
+    //=================================================
+    //DB POST
+    // photoAuth = 'Client-ID f8dcff0ff1e34f2';
+    // clientId = 'f8dcff0ff1e34f2';
+    // var photoReq = {
+    //   method: 'POST',
+    //   url: "https://content.dropboxapi.com/2-beta-2/files/upload",
+    //   data: filePayload,
+    //   // transformRequest: angular.identity,
+    //   headers: {
+    //      'Authorization': 'Bearer zuEjcWgrypQAAAAAAAABPSEvc0s2kb9sJdsvhNW3EuaSwQS-RHfTax24MyW6Xkp2',
+    //      'Content-Type': 'application/octet-stream',
+    //      'Dropbox-API-Arg': {"path":"/ErikAnderson/WebImages"},
+    //      'User-Agent': 'api-explorer-client'
+    //
+    //   }
+    // }
+    // $http(photoReq)
+    // .then(function photoCallback ( response, data ) {
+    //   console.log("photo is being submitted")
+    //   if (response.status == 200) {
+    //     console.log(response)
+    //   };
+    // });
+    // //=================================================
+    //IMGUR POST
+    photoAuth = 'Client-ID f8dcff0ff1e34f2';
+    clientId = 'f8dcff0ff1e34f2';
+    var photoReq = {
+      method: 'POST',
+      url: "https://api.imgur.com/3/image",
+      data: fileVar[0],
+      // transformRequest: angular.identity,
+      headers: {
+         'Authorization': 'Client-ID f8dcff0ff1e34f2',
+      }
+    }
+    $http(photoReq)
+    .then(function photoCallback ( response, data ) {
+      console.log("photo is being submitted")
+      if (response.status == 200) {
+        console.log(response.url)
+      };
+    });
+    // //=================================================
+    // var photoReq = {
+    //   method: 'POST',
+    //   url: "https://" + freshEnd + "/helpdesk/tickets.json",
+    //   data: filePayload,
+    //   // transformRequest: angular.identity,
+    //   headers: {
+    //     transformRequest:angular.identity,
+    //     'Content-Type': undefined,
+    //   }
+    // }
+    // $http(photoReq)
+    // .then(function photoCallback ( response, data ) {
+    //   console.log("photo is being submitted")
+    //   if (response.status == 200) {
+    //     console.log(response)
+    //   };
+    // });
+
+
   }; //END PROCESS FORM
 }]); // END DATA CONTROLLER
