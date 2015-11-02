77b1439223811907468b6fff65065947ee303935
Erik Anderson <itserikanderson@gmail.com>
2015-11-02 10:28:27 -0500

    photos upload to IMGUR, Photos log in Fresh Desk

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -208,32 +208,32 @@ angular.module('authentication', ['ngStorage'])
     };
   });
 
-// GET User Information
-function getUser(){
+  // GET User Information
+  function getUser(){
 
-  var getUser = {
-    method: 'GET',
-    url: 'http://apiv1-1.dtgvending.com/DTG/users/appinit',
-    headers: {
-      'Content-type': 'text/html',
-      'Accept': 'application/json',
-      'XDATE': locdate,
-      'XAUTHENTICATION': xauthentication
-    }
-  };
-  $http(getUser)
-  .then(function successCallback ( response, data ) {
-    console.log("UserInfo")
-    console.dir(response);
-    //Create Session and Store XAUTHENTICATION
-    if (response.status == 200) {
-      userId = response.data
-      this.userInfo = userId;
-      $scope.formData = CurrentSpot;
-      delete spot.cardreader_serial;
+    var getUser = {
+      method: 'GET',
+      url: 'http://apiv1-1.dtgvending.com/DTG/users/appinit',
+      headers: {
+        'Content-type': 'text/html',
+        'Accept': 'application/json',
+        'XDATE': locdate,
+        'XAUTHENTICATION': xauthentication
+      }
     };
-  })
-};
+    $http(getUser)
+    .then(function successCallback ( response, data ) {
+      console.log("UserInfo")
+      console.dir(response);
+      //Create Session and Store XAUTHENTICATION
+      if (response.status == 200) {
+        userId = response.data
+        this.userInfo = userId;
+        $scope.formData = CurrentSpot;
+        delete spot.cardreader_serial;
+      };
+    })
+  };
   // we will store all of our form data in this object
   // $scope.formData = {};
   $scope.locations = $scope.locations;
@@ -245,7 +245,7 @@ function getUser(){
     angular.forEach($scope.files, function(file){
       filePayload.append('file', file)
     })
-  // BIND FILES TO SCOPE
+    // BIND FILES TO SCOPE
     dataDate = new Date();
     pay = "input="
     load = angular.toJson($scope.formData);
@@ -263,8 +263,7 @@ function getUser(){
     locPar = $scope.formData.pars;
     custId = userId.customer;
 
-    // //=================================================
-    //IMGUR POST
+    //IMGUR POST//=================================================
     photoAuth = 'Client-ID f8dcff0ff1e34f2';
     clientId = 'f8dcff0ff1e34f2';
     var photoReq = {
@@ -273,7 +272,7 @@ function getUser(){
       data: fileVar[0],
       // transformRequest: angular.identity,
       headers: {
-         'Authorization': 'Client-ID f8dcff0ff1e34f2',
+        'Authorization': 'Client-ID f8dcff0ff1e34f2',
       }
     }
     $http(photoReq)
@@ -287,7 +286,7 @@ function getUser(){
       };
     });
 
-    //Final PUT Request
+    //Final PUT Request ============================================
     dataAuth = username + ":" + md5.createHash(password + dataDate + pay + load);
     var dataReq = {
       method: 'PUT',
@@ -313,44 +312,44 @@ function getUser(){
       };
     });
 
-    // FRESH DESK FOR HANDLING PHOTOS AND CREATING TICKETS
-function freshFunc (){
-    var apiKey = 'Duw1oQU6YazvCtT5cynJ:x';
-    Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-    freshKey = Base64.encode(apiKey);
-    var freshData = {
-      'helpdesk_ticket':{
-        'group_id': 5000252927,
-        'custom_field': {'customer_id_142177': custId},
-        'email': 'example@example.com',
-        'subject': 'TICKET TEST',
-        'description': "Location #" + locId + " " + locName + " has been updated via the Setup Tool by " + username + " \n \n Address:\n" + locAddress + "\n" + locAddressTwo + "\n" + locCity + ", " + locState + ", " + locZip + "\nMake: " + locMake + "\nModel: " + locModel + "\nFirmware: " + locFirmware + "\n\n Par Values: \n" + locPar + "\n Photo Link: " + photoObj.data.link,
-        // 'attachments': {'resource': {'file': filePayload, content_type:'image/png'}}
-        // '[attachments][][resource]': {file: 'logo.png', content_type: 'image/png'}
-      },
-      'attachement': {filePayload}
-    };
+    // FRESH DESK FOR HANDLING PHOTOS AND CREATING TICKETS ===========================================
+    function freshFunc(){
+      var apiKey = 'Duw1oQU6YazvCtT5cynJ:x';
+      Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+      freshKey = Base64.encode(apiKey);
+      var freshData = {
+        'helpdesk_ticket':{
+          'group_id': 5000252927,
+          'custom_field': {'customer_id_142177': custId},
+          'email': 'example@example.com',
+          'subject': 'TICKET TEST',
+          'description': "Location #" + locId + " " + locName + " has been updated via the Setup Tool by " + username + " \n \n Address:\n" + locAddress + "\n" + locAddressTwo + "\n" + locCity + ", " + locState + ", " + locZip + "\nMake: " + locMake + "\nModel: " + locModel + "\nFirmware: " + locFirmware + "\n\n Par Values: \n" + locPar + "\n\n Photo Link: " + photoObj.data.link,
+          // 'attachments': {'resource': {'file': filePayload, content_type:'image/png'}}
+          // '[attachments][][resource]': {file: 'logo.png', content_type: 'image/png'}
+        },
+        'attachement': {filePayload}
+      };
       freshEnd = 'vagabondvending.freshdesk.com';
       freshReq = {
-      method: 'POST',
-      url: "https://" + freshEnd + "/helpdesk/tickets.json",
-      data: freshData,
-      // transformRequest: angular.identity,
-      headers: {
-        'Accept': '*/*',
-        'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + freshKey
+        method: 'POST',
+        url: "https://" + freshEnd + "/helpdesk/tickets.json",
+        data: freshData,
+        // transformRequest: angular.identity,
+        headers: {
+          'Accept': '*/*',
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic ' + freshKey
+        }
       }
-    }
-    $http(freshReq)
-    .then(function freshCallback ( response, data ) {
-      console.log("freshdesk is being submitted")
-      if (response.status == 200) {
-        console.log("freshdesk was Successful!")
-        console.log(freshKey)
-      };
-    });
-  };
+      $http(freshReq)
+      .then(function freshCallback ( response, data ) {
+        console.log("freshdesk is being submitted")
+        if (response.status == 200) {
+          console.log("freshdesk was Successful!")
+          console.log(freshKey)
+        };
+      });
+    };
     //=================================================
     //DB POST
     // photoAuth = 'Client-ID f8dcff0ff1e34f2';
